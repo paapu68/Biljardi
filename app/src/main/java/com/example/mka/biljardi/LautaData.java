65266fea90d895ae91ad0ae4423451f8d4e08747
paapu68 <markus.kaukonen@iki.fi>
2015-12-28 17:14:20 +0200

    voimia4

--- app/src/main/java/com/example/mka/biljardi/LautaData.java
+++ app/src/main/java/com/example/mka/biljardi/LautaData.java
@@ -30,9 +30,9 @@ public class LautaData {
     
     
     public LautaData(){
-        this.maxSiirtyma = 0.01f;
+        this.maxSiirtyma = 0.005f;
         this.maxNopeus = 0.01f;
-        this.maxKiihtyvyys = 0.1f;
+        this.maxKiihtyvyys = 0.01f;
         this.dt = 0.0001f;  // aika-askel
         this.minLautaX = 0.0f;
         this.minLautaY = 0.0f;
--- app/src/main/java/com/example/mka/biljardi/Liike.java
+++ app/src/main/java/com/example/mka/biljardi/Liike.java
@@ -6,6 +6,8 @@
 
 package com.example.mka.biljardi;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 
 /**
@@ -22,10 +24,11 @@ import java.util.ArrayList;
  *
  */
 
-public class Liike{
+public class Liike {
     LautaData lautadata = new LautaData();
     private float maxSiirtyma, maxNopeus, maxKiihtyvyys;
     private boolean stopMotion;
+
     public Liike() {
         this.maxSiirtyma = 10.0f;
         this.maxNopeus = 10.0f;
@@ -35,20 +38,26 @@ public class Liike{
 
 
     // liikkuvatko pallot enää?
-    public boolean getPallotLiikkuu(){
+    public boolean getPallotLiikkuu() {
+        Log.i("maxSiirtyma", String.valueOf(maxSiirtyma));
+        Log.i("maxNopeus", String.valueOf(maxNopeus));
+        Log.i("maxKiihtyvyys", String.valueOf(maxKiihtyvyys));
+
+        //if ((this.maxSiirtyma < lautadata.maxSiirtyma)
+        //        & (this.maxNopeus < lautadata.maxNopeus)
+        //        & (this.maxKiihtyvyys < lautadata.maxKiihtyvyys)){
+        //    return false;
         if ((this.maxSiirtyma < lautadata.maxSiirtyma)
-                & (this.maxNopeus < lautadata.maxNopeus)
-                & (this.maxKiihtyvyys < lautadata.maxKiihtyvyys)){
+                & (this.maxKiihtyvyys < lautadata.maxKiihtyvyys)) {
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
 
-// tehdään aika-askel ja päivitetään x ja y ja vx ja vy
-    public void update(long fps, Pallot pallot){
-        float dt = 1.0f/fps;
+    // tehdään aika-askel ja päivitetään x ja y ja vx ja vy
+    public void update(long fps, Pallot pallot) {
+        float dt = 1.0f / fps;
 
         ArrayList<Pallo> p1 = pallot.getPallotArray();
         // uudet paikat
@@ -57,15 +66,15 @@ public class Liike{
             float xold = pallo1.getPalloX();
             float yold = pallo1.getPalloY();
 
-            float xnew = pallo1.getPalloX() + pallo1.getPalloVX()*dt
+            float xnew = pallo1.getPalloX() + pallo1.getPalloVX() * dt
                     + 0.5f * pallo1.getPalloAX() * dt * dt;
-            float ynew = pallo1.getPalloY() + pallo1.getPalloVY()*dt
+            float ynew = pallo1.getPalloY() + pallo1.getPalloVY() * dt
                     + 0.5f * pallo1.getPalloAY() * dt * dt;
             pallo1.setPalloX(xnew);
             pallo1.setPalloY(ynew);
-            float siirtyma = (float) Math.sqrt((xold-xnew)*(xold-xnew)
-                    + (yold-ynew)*(yold-ynew));
-            if (siirtyma > this.maxSiirtyma){
+            float siirtyma = (float) Math.sqrt((xold - xnew) * (xold - xnew)
+                    + (yold - ynew) * (yold - ynew));
+            if (siirtyma > this.maxSiirtyma) {
                 this.maxSiirtyma = siirtyma;
             }
         }
@@ -75,27 +84,26 @@ public class Liike{
         // voimat uusissa paikoissa
         this.nollaaVoimat(pallot);
         //lisaavoimat.lisaaCoulombVoimatBiljardiPallot(pallot);
-        //this.lisaaHardCoreVoimat(pallot);
+        this.lisaaHardCoreVoimat(pallot);
         this.lisaaKitka(pallot);
 
         // uudet nopeudet
         for (Pallo pallo1 : p1) {
             float vxnew = pallo1.getPalloVX()
-                        + 0.5f * (pallo1.getPalloFX()/lautadata.getPallonMassa()
-                        + pallo1.getPalloAX()) * dt;
+                    + 0.5f * (pallo1.getPalloFX() / lautadata.getPallonMassa()
+                    + pallo1.getPalloAX()) * dt;
             float vynew = pallo1.getPalloVY()
-                        + 0.5f * (pallo1.getPalloFY()/lautadata.getPallonMassa()
-                        + pallo1.getPalloAY()) * dt;
+                    + 0.5f * (pallo1.getPalloFY() / lautadata.getPallonMassa()
+                    + pallo1.getPalloAY()) * dt;
             pallo1.setPalloVX(vxnew);
             pallo1.setPalloVY(vynew);
         }
         // uudet kiihtyvyydet
         for (Pallo pallo1 : p1) {
-            pallo1.setPalloAX(pallo1.getPalloFX()/lautadata.getPallonMassa());
-            pallo1.setPalloAY(pallo1.getPalloFY()/lautadata.getPallonMassa());
+            pallo1.setPalloAX(pallo1.getPalloFX() / lautadata.getPallonMassa());
+            pallo1.setPalloAY(pallo1.getPalloFY() / lautadata.getPallonMassa());
         }
     }
-
 
 
     /**
@@ -105,46 +113,86 @@ public class Liike{
      * fx = coulombsConstant * q1 * q2 * dx / (r²)
      * fy = coulombsConstant * q1 * q2 * dy / (r²)
      * yksiköt coulombi, metri, kilogramma
+     *
      * @param pallot Pallot jotka vuorovaikuttavat keskenään.
      */
     public void lisaaCoulombVoimatBiljardiPallot(Pallot pallot) {
         float dx, dy, d2;
-        final float coulombsConstant = 8.987551787368f*1000000000f;
-        
+        final float coulombsConstant = 8.987551787368f * 1000000000f;
+
         ArrayList<Pallo> p1 = pallot.getPallotArray();
         for (Pallo pallo1 : p1) {
             for (Pallo pallo2 : p1) {
                 dx = pallo1.getPalloX() - pallo2.getPalloX();
                 dy = pallo1.getPalloY() - pallo2.getPalloY();
-                d2 = (float) Math.sqrt(dx*dx + dy*dy);
+                d2 = (float) Math.sqrt(dx * dx + dy * dy);
                 float varaus1 = pallo1.getPalloVaraus();
                 float varaus2 = pallo2.getPalloVaraus();
                 if (d2 > 0.01) {
-                    pallo1.lisaaPalloFX(coulombsConstant *  
-                        varaus1 * varaus2 * dx / (d2));
-                    pallo1.lisaaPalloFY(coulombsConstant *  
-                        varaus1 * varaus2 * dy / (d2));
+                    pallo1.lisaaPalloFX(coulombsConstant *
+                            varaus1 * varaus2 * dx / (d2));
+                    pallo1.lisaaPalloFY(coulombsConstant *
+                            varaus1 * varaus2 * dy / (d2));
                 }
             }
         }
-    }        
-    
-    
-       
-    /**
-     * Lisätään pallojen kiihtyvyyksiin
-     * pallojen overlap vuorovaikutus.
-     * Eli kun pallot meinaavat mennä päällekkäin sen estää
-     * Lennard Jones repulsio
-     * @see http://en.wikipedia.org/wiki/Lennard-Jones_potential
-     * Tässä vain repulsiivine C12 termi.
-     * @param pallot Pallot jotka vuorovaikuttavat keskenään.
-     */
+    }
+
+    // liikemäärän säilymisen avulla tehty törmäys
     public void lisaaHardCoreVoimat(Pallot pallot) {
+        // Pallojen törmäys liikemäärän säilymisen avulla
+        // https://en.wikipedia.org/wiki/Elastic_collision#Two-dimensional_collision_with_two_moving_objects
+
+
+        ArrayList<Pallo> p1 = pallot.getPallotArray();
+        p1 = pallot.getPallotArray();
+        int imax = pallot.getPallotArray().size();
+        for (int i = 0; i < imax; i++){
+            for (int j = i; j < imax; j++) {
+                Pallo pallo1, pallo2;
+                pallo1 = p1.get(i);
+                pallo2 = p1.get(j);
+                // ovatko samaan suuntaan? (nopeuksien pistetulo > 0)
+                float vdot = pallo1.getPalloVX() * pallo2.getPalloVX() + pallo1.getPalloVY() * pallo2.getPalloVY();
+                float dx = pallo1.getPalloX() - pallo2.getPalloX();
+                float dy = pallo1.getPalloY() - pallo2.getPalloY();
+                float d = (float) Math.sqrt(dx * dx + dy * dy);
+                if ((vdot >= 0f) & (d < lautadata.getPallonHalkaisija() * 2f)) {
+
+                    float dvx = pallo1.getPalloVX() - pallo2.getPalloVX();
+                    float dvy = pallo1.getPalloVY() - pallo2.getPalloVY();
+                    float v1x = pallo1.getPalloVX() - dx * (dvx * dx + dvy * dy) / (d * d);
+                    float v1y = pallo1.getPalloVY() - dy * (dvx * dx + dvy * dy) / (d * d);
+                    float v2x = -v1x;
+                    float v2y = -v1y;
+                    pallo1.setPalloVX(v1x);
+                    pallo1.setPalloVY(v1y);
+                    pallo2.setPalloVX(v2x);
+                    pallo2.setPalloVY(v2y);
+                }
+            }
+        }
+
+    }
+
+
+
+
+        /**
+         * Lisätään pallojen kiihtyvyyksiin
+         * pallojen overlap vuorovaikutus.
+         * Eli kun pallot meinaavat mennä päällekkäin sen estää
+         * Lennard Jones repulsio
+         * @see http://en.wikipedia.org/wiki/Lennard-Jones_potential
+         * Tässä vain repulsiivine C12 termi.
+         * @param pallot Pallot jotka vuorovaikuttavat keskenään.
+         */
+    public void oldLisaaHardCoreVoimat(Pallot pallot) {
         float dx, dy, d, d10;
-        final float epsilon = 1f-13;
+        //final float epsilon = 1f-13;
+        final float epsilon = 1f-16;
         //LautaData lautadata = new LautaData();
-        final float minDist = lautadata.getPallonHalkaisija() *2.0f;
+        final float minDist = lautadata.getPallonHalkaisija() *0.5f;
         
         ArrayList<Pallo> p1 = pallot.getPallotArray();
         for (Pallo pallo1 : p1) {
@@ -171,7 +219,7 @@ public class Liike{
     */
    public void lisaaKitka(Pallot pallot) {
         float vx, vy, massa, kitkaVoima;
-        final float kitkaKerroin = 0.01f;
+        final float kitkaKerroin = 0.05f;
         final float gravitaatioVakio = 9.81f;
         //LautaData lautadata = new LautaData();
         
@@ -180,7 +228,8 @@ public class Liike{
                 vx = pallo.getPalloVX();
                 vy = pallo.getPalloVY();
                 massa = lautadata.getPallonMassa();
-                kitkaVoima = kitkaKerroin * massa * gravitaatioVakio;
+                //kitkaVoima = kitkaKerroin * massa * gravitaatioVakio;
+                kitkaVoima = kitkaKerroin * (float) Math.sqrt(vx*vx + vy*vy);
                 pallo.lisaaPalloFX(-this.getYksikkoVektoriX(vx, vy)*
                         kitkaVoima);            
                 pallo.lisaaPalloFY(-this.getYksikkoVektoriY(vx, vy)*
